dd8e8ea4ce887137e6c23b351853b9ae832d71e8
restorius <summerjamrestor@web.de>
2017-07-12 12:05:25 +0200

    Aktueller Stand
    Flying to the Moon , Doge never die!!!

---
+++ Spiel/Assets/OwnScripts/CoinBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class CoinBehavior : MonoBehaviour {
+	int value = 100;
+	public int points=0;
+	public Text content;
+	public CoinBehavior coin;
+
+
+	// Use this for initialization
+	void Start () {
+		coin = gameObject.GetComponent<CoinBehavior>();	
+		content.text = "Punkte: ";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.content.text = points.ToString();
+	}
+
+	void OnTriggerEnter ( )
+	{
+		GameObject.Destroy ( gameObject ) ;
+		points += value;
+
+
+	}
+}
---
+++ Spiel/Assets/OwnScripts/PointLabel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointLabel : MonoBehaviour {
+	
+	public Text content;
+	Object TextObject;
+	// Use this for initialization
+	void Start () {
+		 TextObject = GameObject.Find ("CoinBehavior");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		content.text = TextObject.Update();
+	}
+}
